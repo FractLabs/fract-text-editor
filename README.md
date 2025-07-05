88b934e5268ec8d1fb5682117cdce91985ee9f3e
0Purple <theredblackofc@gmail.com>
2025-07-05 19:35:45 -0300

    Prototype testing

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Fract Text Editor
 
 The new "amazing" text editor for Linux and Windows.
+
+It's in prototype; feel free to contribute.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+use std::io::Write;
+use std::fs::OpenOptions;
 fn main() {
-    println!("Hello, world!");
+    let file_to_write = "file.txt";
+    let mut write_options = OpenOptions::new()
+    .append(true)
+    .create(true)
+    .write(true)
+    .open(file_to_write)
+    .expect("Error");
+    let mut to_write = "teste breaaaaaaaaaad";
+    let mut line = 1;
+    println!("Fract Text Editor");
+    println!("Press CTRL + C to exit.");
+    // {}
+    // ^ ela vai guardar tudo o que tem dentro dela
+    // {"ola", "ola1", "ola2"}
+    let mut texts: Vec<String> = Vec::new();
+    loop {
+        print!("{line}. ");
+        io::stdout().flush().unwrap();
+        let mut text = String::new();   
+
+        io::stdin().read_line(&mut text);
+        line += 1;
+
+        texts.push(format!("{text}"));
+        writeln!(write_options, "{}", &text.trim()).expect("fail in writing");
+        //write_options.write_all("{to_write}".as_bytes())?;
+    }
 }
