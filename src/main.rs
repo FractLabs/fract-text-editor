e2bedb5b57f27c7fb3fcd0d68c95dd3ab904799e
0Purple <theredblackofc@gmail.com>
2025-07-06 13:53:34 -0300

    UI Prototype

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,48 @@ use std::io::Write;
 use std::fs;
 use std::fs::OpenOptions;
 use slint::slint;
+use rfd::FileDialog;
+use std::path::PathBuf;
 
 slint! {
-    import { HorizontalBox, Button } from "std-widgets.slint";
-    export component FractTextEditor inherits Window {
+import { VerticalBox, HorizontalBox, Button, TextEdit } from "std-widgets.slint";
+export component FractTextEditor inherits Window {
+        in-out property <bool> editing: false;
         title: "Fract Text Editor";
-        min-height: 300px;
-        min-width: 500px;
-        max-height: 600px;
-        max-width: 800px;
+        max-height: 1080px;
+        max-width: 1920px;
+        preferred-height: 256px;
+        preferred-width: 456px;
+
+        VerticalBox {
+
+            if (root.editing == true):
+                TextEdit {
+                    font-size: 8px;
+                    width: parent.width;
+                    height: 300px;
+                    wrap: word-wrap;
+                    enabled: true;
+                    read-only: false;
+                    horizontal-alignment: left;
+                }
+
+
+            Text {
+                text: "Welcome to Fract Text Editor!";
+                font-family: "bold";
+                font-size: 24px;
+                horizontal-alignment: center;
+                vertical-alignment: center;
+
+            }
+            Text {
+                text: "Press New to create a new text file or press Open to open an existing text file.";
+                font-size: 12px;
+                horizontal-alignment: center;
+                vertical-alignment: center;
+            }
+        }
 
         HorizontalBox {
             Button {
@@ -24,6 +57,9 @@ slint! {
                 text: "Open";
                 width: 60px;
                 height: 40px;
+                clicked => {
+                    root.editing = true;
+                }
             } 
             padding: 5px;
             spacing: 30px;
@@ -40,4 +76,19 @@ fn main() {
     .write(true)
     .open(file_to_write)
     .expect("Error");
+
+    select_file_ui();
+}
+fn print_from_rs() {
+    println!("Hello from Rust!")
+}
+
+fn select_file_ui() -> Option<PathBuf> {
+    if let Some(selected_file) = FileDialog::new().pick_file() {
+        println!("File: {:?}", &selected_file.display());
+        Some(selected_file)
+    } else {
+        println!("None selected");
+        None
+    }
 }
